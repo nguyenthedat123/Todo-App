fe2f77a071196f185e0ab906a74d510f097deb61
The Dat <nguyenthedat0207@gmail.com>
2021-07-01 18:32:53 +0700

    Ghi chú về commit

--- TodoApp/TodoApp/ManHinhChinhViewController.swift
+++ TodoApp/TodoApp/ManHinhChinhViewController.swift
@@ -7,36 +7,34 @@
 
 import UIKit
 
-class ManHinhChinhViewController: UIViewController , UITableViewDelegate , UITableViewDataSource {
+final class ManHinhChinhViewController: UIViewController {
+    
+    //MARK: - @IBOutlet
     @IBOutlet weak var table: UITableView!
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.table.delegate = self
+        self.table.dataSource = self
+    }
+}
+   
+//MARK: - UITableViewDelegate
+extension ManHinhChinhViewController: UITableViewDelegate {
 
+}
+
+//MARK: - UITableViewDataSource
+extension ManHinhChinhViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         guard let cell = table.dequeueReusableCell(withIdentifier: "Cell") else {
             return UITableViewCell()
         }
-        return  UITableViewCell()
+        cell.textLabel?.text = "Hello"
+        return cell
     }
-    
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
--- TodoApp/TodoApp/ViewController.swift
+++ TodoApp/TodoApp/ViewController.swift
@@ -33,12 +33,15 @@ final class ViewController: UIViewController {
         
         
     }
+    
+    //MARK: - @IBAction
     @IBAction func btnLogin(_ sender: Any) {
         if (tfUserName.text == "abc123" && tfPassword.text == "123456") {
             let storyboard = UIStoryboard(name: "Main", bundle: nil)
             let vc = storyboard.instantiateViewController(withIdentifier: "manhinh2")
             self.navigationController?.pushViewController(vc, animated: true)
         } else {
+            //lam them nut ok da dismiss UIAlertController
             let alert = UIAlertController(title: "Thông báo", message: "Đăng nhập thất bại", preferredStyle: .alert)
             present(alert, animated: true, completion: nil)
         }
